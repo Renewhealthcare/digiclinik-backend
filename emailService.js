2b1decde62ec721f7358fd73d707ecf43dffbb58
Renewhealthcare <renewhealthcaredavangere@gmail.com>
2024-05-31 10:35:04 +0530

    Add files via upload

---
+++ emailService.js
@@ -0,0 +1,18 @@
+const nodemailer = require("nodemailer");
+const {email,password} = require('../config');
+const password = config.get('password');
+
+// create reusable transporter object using the default SMTP transport
+let EmailService = nodemailer.createTransport({
+    service: 'gmail',
+    user: 'smpt.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        type: 'Login',
+        user: email, // email
+        pass: password, // password
+    },
+});
+
+module.exports = EmailService;
